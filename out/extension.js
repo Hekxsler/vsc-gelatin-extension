cc3158b980eb9d457983903aa636b271eadbb63c
Moritz Hille <moritz.hille@gmx.de>
2022-09-13 12:27:53 +0200

    diag

--- out/extension.js
+++ out/extension.js
@@ -1,27 +1,92 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "gelatin" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('gelatin.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from Gelatin!');
-    });
-    context.subscriptions.push(disposable);
+let diagnosticCollection;
+function activate(ctx) {
+    const collection = vscode.languages.createDiagnosticCollection('go');
+    if (vscode.window.activeTextEditor) {
+        updateDiagnostics(vscode.window.activeTextEditor.document, collection);
+    }
+    ctx.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(editor => {
+        if (editor) {
+            updateDiagnostics(editor.document, collection);
+        }
+    }));
+    const provider = vscode.languages.registerCompletionItemProvider('gelatin', {
+        provideCompletionItems(document, position) {
+            const linePrefix = document.lineAt(position).text.substr(0, position.character);
+            if (linePrefix.endsWith('do.')) {
+                return [
+                    new vscode.CompletionItem('fail', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('next', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('return', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('say', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('skip', vscode.CompletionItemKind.Method),
+                ];
+            }
+            if (linePrefix.endsWith('out.')) {
+                return [
+                    new vscode.CompletionItem('add', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('add_attribute', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('clear_queue', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('create', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('enter', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('enqueue_after', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('enqueue_before', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('enqueue_on_add', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('open', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('replace', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('set_root_name', vscode.CompletionItemKind.Method),
+                ];
+            }
+            return undefined;
+        }
+    }, '.' // triggered whenever a '.' is being typed
+    );
+    ctx.subscriptions.push(collection, provider);
 }
 exports.activate = activate;
-// this method is called when your extension is deactivated
+function createDiagError(document, collection, message, range) {
+    collection.set(document.uri, [{
+            code: '',
+            message: message,
+            range: range,
+            severity: vscode.DiagnosticSeverity.Error,
+            source: '',
+            relatedInformation: []
+        }]);
+}
+function getRange(y, x1, x2, y2) {
+    return new vscode.Range(new vscode.Position(y, x1), new vscode.Position(((y2) ? y2 : y), x2));
+}
+function updateDiagnostics(document, collection) {
+    if (document.fileName.endsWith('.gel')) {
+        let variables = [];
+        for (let x = 0; x < document.lineCount - 1; x++) {
+            let line = document.lineAt(x).text;
+            if (line.startsWith("define")) {
+                var define = line.replace("define ", "").split(" ");
+                let varname = define[0];
+                let regex = define[1];
+                if (varname.match(new RegExp('[a-z0-9_]+'))) {
+                    if () {
+                    }
+                    else {
+                        createDiagError(document, collection, 'Invalid regular expression', getRange(x, line.indexOf(regex), line.lastIndexOf(regex)));
+                    }
+                }
+                else {
+                    createDiagError(document, collection, 'Invalid variable name', getRange(x, 7, 7 + varname.length));
+                }
+            }
+            createDiagError(document, collection, 'Invalid statement', getRange(x, 0, line.length - 1));
+        }
+    }
+    else {
+        collection.clear();
+    }
+}
 function deactivate() { }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,103 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import { stdout } from 'process';
+import { isRegExp } from 'util/types';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gelatin" is now active!');
+let diagnosticCollection: vscode.DiagnosticCollection;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('gelatin.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Gelatin!');
-	});
+export function activate(ctx: vscode.ExtensionContext) {
+  
+  const collection = vscode.languages.createDiagnosticCollection('go');
+	if (vscode.window.activeTextEditor) {
+		updateDiagnostics(vscode.window.activeTextEditor.document, collection);
+	}
+	ctx.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(editor => {
+		if (editor) {
+			updateDiagnostics(editor.document, collection);
+		}
+	}));
 
-	context.subscriptions.push(disposable);
+  const provider = vscode.languages.registerCompletionItemProvider(
+		'gelatin',
+		{
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+				const linePrefix = document.lineAt(position).text.substr(0, position.character);
+				if (linePrefix.endsWith('do.')) {
+					return [
+            new vscode.CompletionItem('fail', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('next', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('return', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('say', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('skip', vscode.CompletionItemKind.Method),
+          ];
+				}
+
+        if (linePrefix.endsWith('out.')) {
+					return [
+            new vscode.CompletionItem('add', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('add_attribute', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('clear_queue', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('create', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('enter', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('enqueue_after', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('enqueue_before', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('enqueue_on_add', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('open', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('replace', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('set_root_name', vscode.CompletionItemKind.Method),
+          ];
+				}
+
+				return undefined;
+			}
+		},
+		'.' // triggered whenever a '.' is being typed
+	);
+
+  ctx.subscriptions.push(collection, provider);
 }
 
-// this method is called when your extension is deactivated
+function createDiagError(document: vscode.TextDocument, collection: vscode.DiagnosticCollection, message: string, range: vscode.Range): void {
+  collection.set(document.uri, [{
+    code: '',
+    message: message,
+    range: range,
+    severity: vscode.DiagnosticSeverity.Error,
+    source: '',
+    relatedInformation: []
+  }]);
+}
+
+function getRange(y: number, x1: number, x2: number, y2?: number): vscode.Range {
+  return new vscode.Range(new vscode.Position(y, x1), new vscode.Position(((y2) ? y2 : y), x2));
+}
+
+
+function updateDiagnostics(document: vscode.TextDocument, collection: vscode.DiagnosticCollection): void {
+	if (document.fileName.endsWith('.gel')) {
+    let variables = []
+    for(let x = 0; x < document.lineCount-1; x++){
+      let line = document.lineAt(x).text
+      if(line.startsWith("define")){
+        var define = line.replace("define ", "").split(" ")
+        let varname = define[0]
+        let regex = define[1]
+        if(varname.match(new RegExp('[a-z0-9_]+'))){
+          if(){
+            
+          }else{
+            createDiagError(document, collection, 'Invalid regular expression', getRange(x, line.indexOf(regex), line.lastIndexOf(regex)))
+          }
+        }else{
+          createDiagError(document, collection, 'Invalid variable name', getRange(x, 7, 7+varname.length))
+        }
+      }
+      
+      createDiagError(document, collection, 'Invalid statement', getRange(x, 0, line.length-1));
+    }
+	} else {
+		collection.clear();
+	}
+}
+
 export function deactivate() {}
